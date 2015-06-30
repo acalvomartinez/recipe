f49bb7b7e17e06a1e461012be98f31873dfd3bc0
Toni <acalvo@e-mutation.com>
2015-06-30 12:09:04 +0200

    xcode project created

---
+++ Favorite Recipe/Favorite Recipe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Favorite Recipe
+//
+//  Created by Toni on 30/06/15.
+//  Copyright (c) 2015 Toni@Ironhack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Favorite Recipe/Favorite Recipe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Favorite Recipe
+//
+//  Created by Toni on 30/06/15.
+//  Copyright (c) 2015 Toni@Ironhack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
